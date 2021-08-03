d8d89dfe57df54c5d6614295ca170bdf1794891d
BJH9 <tfzangin9@gmail.com>
2021-08-03 17:59:32 +0900

    Set fault code and testcases

---
+++ app/src/main/java/edu/handong/csee/tested/FaultDir.java
@@ -0,0 +1,13 @@
+package edu.handong.csee.tested;
+
+public class FaultDir {
+
+	public int adder(int a, int b) {
+		return a+b;
+	}
+	
+	public int subtractor(int a, int b) {
+		return a-b;
+	}
+	
+}
---
+++ app/src/test/java/edu/handong/csee/test/TestCases.java
@@ -0,0 +1,19 @@
+package edu.handong.csee.test;
+import java.util.*;
+import org.junit.Assert;
+
+import edu.handong.csee.tested.*;
+
+
+public class TestCases {
+	
+	public void testAdder() {
+		Assert.assertEquals(2, adder(1, 2));
+		Assert.assertEquals(3, adder(1,2));
+ }
+	public void testSubtractor() {
+		Assert.assertEquals(4, subtractor(6, 2));
+		Assert.assertEquals(1, subtractor(6, 5));
+		Assert.assertEquals(8, subtractor(20, 12));
+	}
+}
